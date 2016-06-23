bce51c4ebd863c4cfe54f4c2f006edc440a47554
박성민 <ifndefdeadmau5@gmail.com>
2016-06-23 09:15:09 +0900

    complete services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,14 @@
 import {Component} from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
-
-
-const HEROES:Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 
 @Component({
   selector: 'my-app',
   directives: [HeroDetailComponent],
+  providers: [HeroService],
   template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
@@ -83,11 +72,22 @@ const HEROES:Hero[] = [
 `]
 })
 
-export class AppComponent {
-  public heroes = HEROES;
+export class AppComponent implements OnInit {
+  heroes: Hero[];
   title = 'Tour of Heroes';
   selectedHero:Hero;
+  constructor(private heroService: HeroService) { }
 
+  ngOnInit() {
+    // this.getHeroes();
+    this.getHeroesSlowly();
+  }
+  getHeroesSlowly() {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
   onSelect(hero:Hero) {
     this.selectedHero = hero;
   }
---
+++ app/hero.service.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by trevor on 2016. 6. 20..
+ */
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+
+@Injectable()
+export class HeroService {
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+
+  getHeroesSlowly() {
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+    );
+  }
+  
+}
