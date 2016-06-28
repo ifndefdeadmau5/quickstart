0d28636dffc828fc09e8e6842db6320f38ec212a
SeongMin Park <ifndefdeadmau5@gmail.com>
2016-06-28 22:15:13 +0900

    Complete routing

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -2,6 +2,7 @@
  * Created by trevor on 16. 6. 23..
  */
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router-deprecated';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -9,12 +10,18 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
-  templateUrl: 'app/dashboard.component.html'
+  templateUrl: 'app/dashboard.component.html',
+  styleUrls: ['app/dashboard.component.css']
+
 })
 
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) {
+  }
+
   ngOnInit() {
     this.heroService.getHeroes()
       .then(heroes => this.heroes = heroes.slice(1, 5));
