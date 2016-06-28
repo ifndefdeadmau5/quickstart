4f81e6c7221a7dba8200317ec008d9223678622a
Trevor Park <ifndefdeadmau5@gmail.com>
2016-06-28 20:38:09 +0900

    working on routing... not completed

---
+++ app/dashboard.component.ts
@@ -0,0 +1,27 @@
+/**
+ * Created by trevor on 16. 6. 23..
+ */
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+
+@Component({
+  selector: 'my-dashboard',
+  templateUrl: 'app/dashboard.component.html'
+})
+
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) { }
+  ngOnInit() {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+  gotoDetail(hero: Hero) {
+    let link = ['HeroDetail', { id: hero.id }];
+    this.router.navigate(link);
+  }
+}
+
